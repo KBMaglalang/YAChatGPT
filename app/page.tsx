fcd9241c0828a2e6a2603b4cf338ba42c8bccc0d
Kurt Maglalang <kbm126@live.com>
2023-09-04 13:29:30 -0700

    feat: update homepage to use constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 
+import {
+  HOMEPAGE_TITLE,
+  HOMEPAGE_CONVERSATION_TITLE,
+  HOMEPAGE_CONVERSATION_DESCRIPTION,
+  HOMEPAGE_PROMPT_TITLE,
+  HOMEPAGE_PROMPT_DESCRIPTION,
+} from "@/lib/constants";
+
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 overflow-hidden text-white">
-      <h1 className="mb-20 text-5xl font-bold">ChatGPT</h1>
+      <h1 className="mb-20 text-5xl font-bold">{HOMEPAGE_TITLE}</h1>
 
       <div className="p-4 m-4 bg-[#434654] rounded-xl max-w-2xl">
-        <h2 className="mb-4 text-3xl font-bold ">Conversations</h2>
-        <p>
-          {`Just click on '+ New Chat' at the bottom of the page to chat with
-          ChatGPT. You can play around with the settings to make the AI chat
-          just the way you like, whether that's more random, more focused, or
-          less repetitive.`}
-        </p>
+        <h2 className="mb-4 text-3xl font-bold ">
+          {HOMEPAGE_CONVERSATION_TITLE}
+        </h2>
+        <p>{HOMEPAGE_CONVERSATION_DESCRIPTION}</p>
       </div>
 
       <div className="p-4 m-4 bg-[#434654] rounded-xl max-w-2xl">
-        <h2 className="mb-4 text-3xl font-bold ">Prompt Templates</h2>
-        <p>
-          {`Using templates with AI like ChatGPT makes chatting easier and the
-          responses better. It's like giving the AI a helping hand to understand
-          you better, so you get the info you need faster and can have a more
-          meaningful chat.`}
-        </p>
+        <h2 className="mb-4 text-3xl font-bold ">{HOMEPAGE_PROMPT_TITLE}</h2>
+        <p>{HOMEPAGE_PROMPT_DESCRIPTION}</p>
       </div>
     </div>
   );
