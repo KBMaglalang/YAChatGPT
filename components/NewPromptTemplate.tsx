3bc9a8dc5ccb6e32a190dea0038b57696cad4823
Kurt Maglalang <kbm126@live.com>
2023-09-18 23:27:40 -0700

    feat: create button to open modal

--- components/NewPromptTemplate.tsx
+++ components/NewPromptTemplate.tsx
@@ -1,54 +1,40 @@
 import React, { useState } from "react";
-import { useSession } from "next-auth/react";
-import { PlusIcon } from "@heroicons/react/24/outline";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-
-import { db } from "@/firebase";
+import { BookOpenIcon } from "@heroicons/react/24/outline";
 
 // components
-import PromptModal from "./PromptEditModal";
+import PromptList from "./PromptList";
 
 function NewPromptTemplate() {
-  const { data: session } = useSession();
   const [modalOpen, setModalOpen] = useState(false);
 
-  /**
-   * Creates a new prompt template.
-   *
-   * @param {string} title - The title of the prompt template.
-   * @param {string} prompt - The content of the prompt template.
-   * @returns {void}
-   */
-  const createNewPromptTemplate = async (title: string, prompt: string) => {
-    const doc = await addDoc(
-      collection(db, "users", session?.user?.email!, "prompt"),
-      {
-        userId: session?.user?.email!,
-        createdAt: serverTimestamp(), // ! don't use local timestamp use server timestamp
-        title: title || "New Prompt",
-        prompt: prompt || "",
-      }
-    );
+  const handleUpdateSettings = async () => {
+    // const doc = await addDoc(
+    //   collection(db, "users", session?.user?.email!, "settings"),
+    //   {
+    //     userId: session?.user?.email!,
+    //     createdAt: serverTimestamp(), // ! don't use local timestamp use server timestamp since you don't know where they are in the world
+    //     title: title || "New Prompt",
+    //     prompt: prompt || "",
+    //   }
+    // );
   };
 
   return (
-    <>
+    <div>
       {/* new prompt button */}
       <div onClick={() => setModalOpen(true)} className="chatRow">
-        <PlusIcon className="w-4 h-4" />
-        <span>New Prompt</span>
+        <BookOpenIcon className="w-4 h-4" />
+        <span className="hidden lg:block">New Prompt</span>
       </div>
 
       {/* prompt modal */}
       {modalOpen && (
-        <PromptModal
+        <PromptList
           setModalOpen={setModalOpen}
-          callback={createNewPromptTemplate}
-          title={""}
-          prompt={""}
+          callback={handleUpdateSettings}
         />
       )}
-    </>
+    </div>
   );
 }
 
