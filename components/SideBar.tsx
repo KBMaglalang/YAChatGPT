a19a602e4e2e08d50ed516af32f1c7e6cbe00bce
Kurt Maglalang <kbm126@live.com>
2023-09-18 23:32:03 -0700

    feat: rearrange side bar components

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -7,12 +7,17 @@ import React, { useState } from "react";
 import { useSession } from "next-auth/react";
 import { collection, orderBy, query } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore"; // ! setups a real time connection to the firebase database
-import { ArrowDownCircleIcon, BookOpenIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowDownCircleIcon,
+  ArrowUpCircleIcon,
+  BookOpenIcon,
+} from "@heroicons/react/24/outline";
 
 import { db } from "@/firebase";
 
 // components
-import NewChat from "./NewChat";
+import CreateNewChatButton from "./CreateNewChatButton";
+// import NewChatButton from "./NewChatButton";
 import ChatRow from "./ChatRow";
 import ModelSelection from "./ModelSelection";
 import SettingsRow from "./SettingsRow";
@@ -20,7 +25,6 @@ import Loading from "./Loading";
 
 function SideBar() {
   const { data: session } = useSession();
-  const [isOpen, setIsOpen] = useState(true);
   const [chats, loading, error] = useCollection(
     session &&
       query(
@@ -29,68 +33,58 @@ function SideBar() {
       )
   );
 
-  const toggleDrawer = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <div
-      className={`flex flex-col p-2 h-screen ${isOpen ? "min-w-[20rem]" : ""}`}
-    >
+    <div className={`flex flex-col p-2`}>
+      <div className={`flex flex-row mt-auto space-x-2 w-full`}>
+        {/* new chat */}
+        <div className="flex-1">
+          <CreateNewChatButton />
+        </div>
+
+        {/* <div className={`chatRow`} onClick={toggleDrawer}>
+          <BookOpenIcon className="w-4 h-4" />
+        </div> */}
+      </div>
+
       {/* loading */}
-      {loading && isOpen && (
-        <div className="flex justify-center mt-4 h-screen">
+      {loading && (
+        <div className="flex justify-center mt-4">
           <Loading />
         </div>
       )}
 
       {/* cta */}
-      {chats?.empty && isOpen && (
-        <div className="flex flex-col justify-end items-center h-full">
+      {chats?.empty && (
+        <div className="flex flex-col justify-end items-center mt-6 h-full">
+          <ArrowUpCircleIcon className="mx-auto mt-5 w-10 h-10 text-white animate-bounce" />
           <p className="hidden text-xl font-bold text-white truncate md:inline-flex">
             Create New Chats
           </p>
-          <ArrowDownCircleIcon className="mx-auto mt-5 w-10 h-10 text-white animate-bounce" />
         </div>
       )}
 
       {/* chat options */}
-      {isOpen && (
-        <div className="overflow-y-scroll flex-1">
-          <div className="flex flex-col my-2 space-y-2">
-            {/* map through the chatRows */}
-            {!chats?.empty &&
-              chats?.docs.map((chat) => <ChatRow key={chat.id} id={chat.id} />)}
-          </div>
+      <div className="overflow-y-scroll flex-1">
+        <div className="flex flex-col my-2 space-y-2">
+          {/* map through the chatRows */}
+          {!chats?.empty &&
+            chats?.docs.map((chat) => <ChatRow key={chat.id} id={chat.id} />)}
         </div>
-      )}
+      </div>
 
       {/* model selection */}
-      {isOpen && (
+      {/* {isOpen && (
         <div className="hidden mb-2 sm:inline">
           <ModelSelection />
         </div>
-      )}
+      )} */}
 
       {/* Settings */}
-      {isOpen && (
+      {/* {isOpen && (
         <div className="mb-2">
           <SettingsRow />
         </div>
-      )}
-
-      <div className={`flex flex-row mt-auto space-x-2 w-full`}>
-        {/* new chat */}
-        {isOpen && (
-          <div className="flex-1">
-            <NewChat />
-          </div>
-        )}
-
-        <div className={`chatRow`} onClick={toggleDrawer}>
-          <BookOpenIcon className="w-4 h-4" />
-        </div>
-      </div>
+      )} */}
     </div>
   );
 }
