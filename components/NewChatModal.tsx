787566a5c51b0a8af8d343cac3df5a1cb12a3045
Kurt Maglalang <kbm126@live.com>
2023-09-18 23:26:45 -0700

    feat: list chats in modal

---
+++ components/NewChatModal.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+// components
+import SideBar from "./SideBar";
+
+type Props = {
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  callback: any;
+};
+
+export default function NewChatModal({ setModalOpen, callback }: Props) {
+  const handleAccept = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    setModalOpen(false);
+  };
+
+  return (
+    <div className="overflow-y-auto fixed inset-0 z-10">
+      {/* handles clicks outside the modal box */}
+      <div
+        className="fixed inset-0 w-full h-full bg-black opacity-40"
+        onClick={(e) => setModalOpen(false)}
+      ></div>
+
+      {/* modal box */}
+      <div className="flex items-center px-4 py-8 min-h-screen">
+        <div className="relative w-full max-w-lg p-4 mx-auto rounded-md shadow-2xl bg-[#121212] shadow-gray-700">
+          {/* settings input or logout */}
+          <div className="mt-3">
+            <div className="flex flex-col mt-2 text-center">
+              <h4 className="mb-2 text-xl font-bold text-white">
+                Conversations
+              </h4>
+
+              {/* list of chats  */}
+              <SideBar />
+            </div>
+          </div>
+
+          {/* user selection */}
+          <div className="gap-2 items-center mt-3 sm:flex">
+            {/* <button
+              className="w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-indigo-600 focus:ring-2 hover:bg-gray-700"
+              onClick={handleAccept}
+            >
+              Accept
+            </button> */}
+            <button
+              className="w-full mt-2 p-2.5 flex-1 text-white rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2 hover:bg-gray-700"
+              onClick={(e) => setModalOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
