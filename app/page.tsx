5a0be08714949eb6808a030138a6234c5398418d
Kurt Maglalang <kbm126@live.com>
2023-09-19 20:54:14 -0700

    feat:update code for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,24 @@ import SideBar from "@/components/SideBar";
 
 function HomePage() {
   return (
-    <div className="flex flex-col justify-center items-center px-2 h-screen text-white lg:overflow-hidden">
-      <h1 className="mb-20 text-5xl font-bold">{HOMEPAGE_TITLE}</h1>
+    <div className="flex flex-col justify-center items-center px-2 text-white lg:overflow-hidden">
+      <h1 className="mb-20 w-full text-5xl font-bold text-center">
+        {HOMEPAGE_TITLE}
+      </h1>
 
-      <div className="p-4 m-4 bg-[#121212] rounded-xl w-1/2 shadow-gray-700 shadow-2xl">
+      <div className="p-4 m-4 bg-[#121212] rounded-xl w-full md:w-1/2 shadow-gray-700 shadow-2xl">
         <h2 className="mb-4 text-3xl font-bold">
           {HOMEPAGE_CONVERSATION_TITLE}
         </h2>
         <p>{HOMEPAGE_CONVERSATION_DESCRIPTION}</p>
       </div>
 
-      <div className="p-4 m-4 bg-[#121212] rounded-xl w-1/2 shadow-gray-700 shadow-2xl">
+      <div className="p-4 m-4 bg-[#121212] rounded-xl w-full md:w-1/2 shadow-gray-700 shadow-2xl">
         <h2 className="mb-4 text-3xl font-bold">{HOMEPAGE_PROMPT_TITLE}</h2>
         <p>{HOMEPAGE_PROMPT_DESCRIPTION}</p>
       </div>
 
-      <div className="p-4 m-4 bg-[#121212] rounded-xl w-2xl max-h-1/6 w-1/2 shadow-gray-700 shadow-2xl">
+      <div className="p-4 m-4 bg-[#121212] rounded-xl w-full md:w-1/2 shadow-gray-700 shadow-2xl">
         <h2 className="mb-4 text-3xl font-bold text-center">Conversations</h2>
 
         <div className="overflow-y-scroll rounded-xl">
