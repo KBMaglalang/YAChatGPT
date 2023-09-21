48b1c3028882ee2daac1d11e9aa4f6a6be7a998b
Kurt Maglalang <kbm126@live.com>
2023-09-21 15:28:58 -0700

    feat: update page to useChat function for text streaming

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import React from "react";
-import { useChat } from "ai/react";
+import React, { useEffect } from "react";
+import { useChat, Message } from "ai/react";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { orderBy, query, addDoc, collection } from "firebase/firestore";
+import useSWR from "swr";
+import { toast } from "react-hot-toast";
+
+import { useStateContext } from "@/lib/context/stateContext";
+import { CHATGPT_DEFAULT } from "@/lib/constants";
 
 // components
 import Chat from "@/components/Chat";
 import ChatInput from "@/components/ChatInput";
+import { db } from "@/firebase";
 
 type Props = {
   params: {
@@ -14,6 +23,13 @@ type Props = {
 };
 
 function ChatPage({ params: { id } }: Props) {
+  // * there was a change on the api endpoints v1 -> v2
+  // useSWR to get models from openai
+  const { data: model } = useSWR("model", {
+    fallbackData: CHATGPT_DEFAULT,
+  });
+  const { data: session } = useSession();
+  const { promptSettings } = useStateContext();
   const {
     messages,
     input,
@@ -22,7 +38,71 @@ function ChatPage({ params: { id } }: Props) {
     handleInputChange,
     handleSubmit,
     stop,
-  } = useChat();
+    setMessages,
+  } = useChat({
+    onFinish: async (message) => {
+      const responseMessage = {
+        ...message,
+
+        maxTokens: promptSettings.maxTokens,
+        temperature: promptSettings.temperature,
+        topP: promptSettings.topP,
+        frequencyPenalty: promptSettings.frequencyPenalty,
+        presencePenalty: promptSettings.presencePenalty,
+        modelName: model,
+
+        user: {
+          _id: "assistant",
+          name: "assistant",
+          avatar:
+            "https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg",
+        },
+      };
+
+      await addDoc(
+        collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+        responseMessage
+      );
+    },
+
+    body: {
+      settings: {
+        maxTokens: promptSettings.maxTokens,
+        temperature: promptSettings.temperature,
+        topP: promptSettings.topP,
+        frequencyPenalty: promptSettings.frequencyPenalty,
+        presencePenalty: promptSettings.presencePenalty,
+        modelName: model,
+      },
+    },
+  });
+
+  const [firebaseMessages, firebaseLoading, firebaseError] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  useEffect(() => {
+    if (error) {
+      toast.error(JSON.parse(error.message).message);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (messages.length == 0 && firebaseMessages?.docs?.length! > 0) {
+      const newMessage = firebaseMessages?.docs?.map((doc) => ({
+        content: doc.data().content,
+        role: doc.data().role,
+        createdAt: doc.data().createdAt,
+        id: doc.data().id,
+      }));
+
+      setMessages(newMessage as Message[]);
+    }
+  }, [firebaseMessages, firebaseLoading]);
 
   return (
     <div className="flex overflow-hidden flex-col items-center w-screen lg:w-2/3">
