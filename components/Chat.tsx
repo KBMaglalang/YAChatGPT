0e49436f20e0adba71f6506670b91e66e3c59e21
Kurt Maglalang <kbm126@live.com>
2023-09-21 15:49:09 -0700

    feat: update to use chat streaming

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,41 +1,18 @@
 "use client";
 
 import React, { useEffect, useRef } from "react";
-import { useSession } from "next-auth/react";
-import { useCollection } from "react-firebase-hooks/firestore";
-import { collection, orderBy, query } from "firebase/firestore";
 import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 
-import { db } from "@/firebase";
+// components
 import Message from "./Message";
 import Loading from "./Loading";
 
 type Props = {
-  chatId: string;
   llmMessages: any;
   llmIsLoading: boolean;
 };
 
-function Chat({ chatId, llmMessages, llmIsLoading }: Props) {
-  console.log("🚀 ~ file: Chat.tsx:20 ~ Chat ~ llmIsLoading:", llmIsLoading);
-  console.log("🚀 ~ file: Chat.tsx:19 ~ Chat ~ llmMessages:", llmMessages);
-
-  const { data: session } = useSession();
-  const [messages, loading, error] = useCollection(
-    session &&
-      query(
-        collection(
-          db,
-          "users",
-          session?.user?.email!,
-          "chats",
-          chatId,
-          "messages"
-        ),
-        orderBy("createdAt", "asc")
-      )
-  );
-
+function Chat({ llmMessages, llmIsLoading }: Props) {
   // Method to scroll to the bottom of a scrollable div container
   // Reference for the scrollable div
   const scrollContainerRef = useRef<null | HTMLDivElement>(null);
@@ -54,14 +31,14 @@ function Chat({ chatId, llmMessages, llmIsLoading }: Props) {
       ref={scrollContainerRef}
     >
       {/* loading */}
-      {loading && (
+      {llmIsLoading && (
         <div className="flex justify-center items-center h-screen">
           <Loading />
         </div>
       )}
 
       {/* cta */}
-      {messages?.empty && (
+      {llmMessages?.empty && (
         <div className="flex flex-col justify-center items-center h-full">
           <p className="mt-10 text-xl font-bold text-center text-white justify">
             Type a prompt below!
@@ -73,10 +50,7 @@ function Chat({ chatId, llmMessages, llmIsLoading }: Props) {
       )}
 
       {/* messages */}
-      {/* {messages?.docs.map((message) => (
-        <Message key={message.id} message={message.data()} />
-      ))} */}
-      {llmMessages.map((message) => (
+      {llmMessages?.map((message) => (
         <Message key={message.id} message={message} />
       ))}
     </div>
