bfe1d13be94fee746db1b783c679968009629967
Kurt Maglalang <kbm126@live.com>
2023-11-16 22:11:56 -0800

    style: add bg color to messages

--- src/components/ChatArea/Message.tsx
+++ src/components/ChatArea/Message.tsx
@@ -18,7 +18,7 @@ function Message({ message }: Props) {
   const isAssistant = message.role === "assistant";
 
   return (
-    <div className={`py-5  rounded-xl `}>
+    <div className={`py-5  rounded-xl bg-base-300`}>
       <div className="flex px-10 mx-auto space-x-5">
         {/* user and chatgpt icons */}
         {isAssistant ? (
@@ -38,7 +38,7 @@ function Message({ message }: Props) {
           {/* message markdown */}
           <div className="flex flex-col flex-wrap items-center">
             <ReactMarkdown
-              className="pt-2 w-full max-w-4xl  break-words prose flex-shrink-1 font-brand-roboto"
+              className="pt-2 w-full max-w-4xl break-words prose flex-shrink-1 font-brand-roboto"
               remarkPlugins={[remarkGfm]}
             >
               {message.content}
