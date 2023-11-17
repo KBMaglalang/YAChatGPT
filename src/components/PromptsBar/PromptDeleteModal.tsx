023926d2bfd3b6140354381a557c49ddcc3eb54e
Kurt Maglalang <kbm126@live.com>
2023-11-16 21:07:30 -0800

    style: update prompt modals to have bg color

--- src/components/PromptsBar/PromptDeleteModal.tsx
+++ src/components/PromptsBar/PromptDeleteModal.tsx
@@ -30,11 +30,11 @@ function PromptDeleteModal({ setModalOpen, callback }: Props) {
 
       {/* modal box */}
       <div className="flex items-center px-4 py-8 min-h-screen">
-        <div className="relative p-4 mx-auto w-full max-w-lg rounded-md shadow-2xl  shadow-gray-700">
+        <div className="relative p-4 mx-auto w-full max-w-lg rounded-md shadow-2xl bg-base-200 shadow-gray-700">
           {/* prompt input */}
           <div className="mt-3">
             <div className="flex flex-col mt-2 text-center">
-              <h4 className="mb-2 text-xl font-bold text-white font-brand-roboto">
+              <h4 className="mb-2 text-xl font-bold  font-brand-roboto">
                 Delete Prompt?
               </h4>
             </div>
@@ -43,15 +43,13 @@ function PromptDeleteModal({ setModalOpen, callback }: Props) {
           {/* user selection */}
           <div className="gap-2 items-center mt-3 sm:flex">
             <button
-              // className="font-brand-roboto w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-indigo-600 focus:ring-2 hover:bg-gray-700"
               className="btn btn-primary font-brand-roboto flex-1"
               onClick={handleAccept}
             >
               Accept
             </button>
             <button
-              // className="font-brand-roboto w-full mt-2 p-2.5 flex-1 text-white rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2 hover:bg-gray-700"
-              className="btn btn-outline text-white font-brand-roboto flex-1"
+              className="btn btn-outline  font-brand-roboto flex-1"
               onClick={(e) => setModalOpen(false)}
             >
               Cancel
--- src/components/PromptsBar/PromptEditModal.tsx
+++ src/components/PromptsBar/PromptEditModal.tsx
@@ -40,11 +40,11 @@ function PromptEditModal({
 
       {/* modal box */}
       <div className="flex items-center px-4 py-8 min-h-screen">
-        <div className="relative p-4 mx-auto w-full max-w-lg rounded-md shadow-2xl  shadow-gray-700">
+        <div className="relative p-4 mx-auto w-full max-w-lg rounded-md shadow-2xl bg-base-200 shadow-gray-700">
           {/* prompt input */}
           <div className="mt-3">
             <div className="flex flex-col mt-2 text-center">
-              <h4 className="mb-2 text-xl font-bold text-white font-brand-roboto">
+              <h4 className="mb-2 text-xl font-bold  font-brand-roboto">
                 Prompt Settings
               </h4>
               <input
@@ -52,14 +52,14 @@ function PromptEditModal({
                 type="text"
                 value={promptTitle}
                 onChange={(e) => setPromptTitle(e.target.value)}
-                className="p-2 my-2 text-white rounded-lg  focus:outline-none font-brand-roboto"
+                className="p-2 my-2  rounded-lg  focus:outline-none font-brand-roboto"
                 placeholder="Prompt Title"
               />
               <textarea
                 value={promptTextArea}
                 onChange={(e) => setPromptTextArea(e.target.value)}
                 rows={8}
-                className="p-2 my-2 text-white rounded-lg resize-none  focus:outline-none font-brand-roboto"
+                className="p-2 my-2  rounded-lg resize-none  focus:outline-none font-brand-roboto"
                 placeholder="Type your prompt here"
               />
             </div>
@@ -68,15 +68,13 @@ function PromptEditModal({
           {/* user selection */}
           <div className="gap-2 items-center mt-3 sm:flex">
             <button
-              // className="font-brand-roboto w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-indigo-600 focus:ring-2 hover:bg-gray-700"
               className="btn btn-primary font-brand-roboto flex-1"
               onClick={handleAccept}
             >
               Accept
             </button>
             <button
-              // className="font-brand-roboto w-full mt-2 p-2.5 flex-1 text-white rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2 hover:bg-gray-700"
-              className="btn btn-outline text-white font-brand-roboto flex-1"
+              className="btn btn-outline  font-brand-roboto flex-1"
               onClick={(e) => setModalOpen(false)}
             >
               Cancel
