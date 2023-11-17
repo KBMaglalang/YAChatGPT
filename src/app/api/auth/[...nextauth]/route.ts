d275bd2b7a14d5577894f478231e5075cee948a4
Kurt Maglalang <kbm126@live.com>
2023-11-16 22:39:39 -0800

    refactor: remove dead code from auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
 import NextAuth from "next-auth";
-// import { authOptions } from "@/config/auth/auth";
-// import { authOptions } from "@/config/auth";
 import { authOptions } from "@/config/auth/auth";
 
 const handler = NextAuth(authOptions);
